776ebfd3f60d80ebc0aa3f9635d31a7b5c21e260
heather dougherty <heatherdougherty1234@me.com>
2020-12-14 19:56:24 -0500

    Implemented FilterNumbersMethod

--- LargeNumbers/LargeNumberFilter.cs
+++ LargeNumbers/LargeNumberFilter.cs
@@ -1,18 +1,33 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace LargeNumbers
 {
 	public class LargeNumberFilter
 	{
 		/// <summary>
-		/// Returns an array containing all of the elements of the input array that are greater than the maxValue parameter 
+		/// Returns an array containing all of the elements of the input array that are greater than or equal the maxValue parameter 
 		/// </summary>
 		/// <param name="values"></param>
 		/// <param name="maxValue"></param>
 		/// <returns></returns>
 		public int[] FilterNumbers(int[] values, int maxValue)
 		{
-			throw new NotImplementedException();
+			if(values == null)
+            {
+				return new int[0];
+            }
+
+			var largeValues = new List<int>();
+
+			foreach(int value in values)
+            {
+				if(value >= maxValue)
+                {
+					largeValues.Add(value);
+				}
+            }
+			
+			return largeValues.ToArray();
 		}
 	}
 }
